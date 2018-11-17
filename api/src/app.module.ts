201bd52119ad242ee6d5f1678b40f0cc2d98156f
ShinDarth <borzifrancesco@gmail.com>
2018-11-17 17:45:00 +0100

    Load new entity modules

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -2,8 +2,15 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { DepartmentModule } from './department/department.module';
+import { CourseModule } from './course/course.module';
+import { EvaluationModule } from './evaluation/evaluation.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(), DepartmentModule],
+  imports: [
+    TypeOrmModule.forRoot(),
+    DepartmentModule,
+    CourseModule,
+    EvaluationModule,
+  ],
 })
 export class AppModule {}
